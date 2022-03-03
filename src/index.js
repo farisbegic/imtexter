b552089cd968ceb40f37628c8692b3f7b7d1484e
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-03-03 20:43:46 +0100

    Implemented scrollbar and added favicon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./components/Header/Header";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Scrollbars } from 'react-custom-scrollbars';
 import Home from "./pages/Home";
 import Wrapper from "./components/Wrapper/Wrapper";
 import './global.css'
@@ -9,12 +10,14 @@ import "react-widgets/styles.css";
 
 ReactDOM.render(
     <BrowserRouter>
-        <Wrapper>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Wrapper>
+        <Scrollbars style={{height: "100vh"}}>
+            <Wrapper>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Wrapper>
+        </Scrollbars>
     </BrowserRouter>,
   document.getElementById('root')
 );
